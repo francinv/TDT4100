a454314a2cc2f5ce9e575075f282021e0b818703
unknown <borgeha@ntnu.no>
2023-02-05 21:43:04 +0100

    pre_f

--- foreksempel/src/main/java/uke6/fasit/File.java
+++ foreksempel/src/main/java/uke6/fasit/File.java
@@ -14,16 +14,44 @@ public class File {
     }
 
     public File(String name, Folder parent) {
-        //TODO
+        if (parent == null)
+            throw new IllegalStateException("En fil må ha en parent.");
+        this.parent = parent;
+        this.name = name;
+        parent.addFile(this);
     }
 
     
-    @Override
-    public String toString() {
-        return getName();
-    }
+	@Override
+	public String toString() {
+		return parent+"/"+getName();
+	}
+
+
 
     public void move(Folder destination) {
-    // TODO
+        parent.removeFile(this);
+        destination.addFile(this);
+        this.parent = destination;
     }
+
+    public static void main(String[] args) {
+        Folder home = new Folder("home",null);
+        Folder andreas = new Folder("andreas",home);
+        Folder borgeh = new Folder("borgeh",home);
+        Folder carsten = new Folder("carsten",home);
+        Folder adiv = new Folder("adiv",andreas);
+        Folder bdiv = new Folder("bdiv",borgeh);
+        Folder btmp = new Folder("btmp",borgeh);
+        Folder cdiv = new Folder("cdiv",carsten);
+        File cfil = new File("CarstenFil.txt", carsten);
+        // System.out.println(cfil);
+        home.printTree();
+
+        cfil.move(borgeh);
+        home.printTree();
+        home.findFirst("hei");
+        Object o = home.findFirst("CarstenFil.txt");
+        System.out.println("Funnet: "+o);
+        }
 }
--- foreksempel/src/main/java/uke6/fasit/Folder.java
+++ foreksempel/src/main/java/uke6/fasit/Folder.java
@@ -3,17 +3,30 @@ package uke6.fasit;
 import java.util.ArrayList;
 import java.util.Collection;
 
+
 public class Folder {
     String name;
     Folder parent;
-    Collection<Folder> folders;
+    Collection<Folder> folders = new ArrayList<>();
+    Collection<File> files = new ArrayList<>();
+
 
     public Folder(String name, Folder parent) {
         this.name = name;
         this.parent = parent;
-        folders = new ArrayList<>();
+        if (parent != null) 
+            parent.addFolder(this);
+
     }
 
+
+    public void addFile(File file) {
+        this.files.add(file);
+    }
+
+    public void removeFile(File file) {
+        this.files.remove(file);
+    }
 
     public void addFolder(Folder folder) {
         this.folders.add(folder);
@@ -32,10 +45,96 @@ public class Folder {
     }
 
 
-    @Override
-    public String toString() {
-        return name;
+    private Collection<Folder> getFolders() {
+        return this.folders;
     }
+
+	@Override
+	public String toString() {
+		String tmp = "/" + name;
+		if (parent != null)
+			tmp = parent.toString() + tmp;
+		return tmp;
+	}
+
+
+    public void printTree() {
+        System.out.println(this);
+        for (File file : files) {
+            System.out.println(file);
+        }
+        for (Folder folder : folders) {
+            folder.printTree();
+        }
+    }
+
+
+    private boolean contains(Folder destination) {
+        System.out.println(this+ " contains "+destination);
+        if (this.equals(destination)) return true;
+        for (Folder folder : folders) {
+            if (folder.contains(destination)) return true;
+        }
+        return false;
+    }
+
+    private boolean contains2(Folder destination) {
+        if (destination != null)
+            System.out.println(this+ " contains2 "+destination);
+        if (destination == null) return false;
+        if (destination == this) return true;
+        return this.contains2(destination.getParent()); 
+    }
+
+
+    public void move(Folder destination) {
+
+        if (this.contains(destination))
+            throw new IllegalStateException("Loop de loop");
+        parent.removeFolder(this);
+        this.parent = destination;
+        parent.addFolder(this);
+    }
+
+    Object findFirst(String pattern) {
+		for (File file : files) {
+			if (file.name.equals(pattern))
+				return file;
+		}
+		
+		for (Folder folder : folders) {
+            if (folder.name.equals(pattern))
+            return folder;
+		}
+		
+		for (Folder folder : folders) {
+			Object found = folder.findFirst(pattern);
+			if (found != null)
+				return found;
+		}
+		
+		return null;
+		
+	}
+
+    private void findAll(String pattern, Collection<File> hits) {
+        for (File file : files) {
+            if (file.getName().equals(pattern))
+                hits.add(file);
+        }
+
+        for (Folder folder : folders) {
+            folder.findAll(pattern, hits);
+        }
+    }
+
+
+    public Collection<File> findAll(String pattern) {
+        Collection<File> tmp = new ArrayList<>();
+        findAll(pattern, tmp);
+        return tmp;
+    }
+
 
     public static void main(String[] args) {
         Folder home = new Folder("home",null);
@@ -46,7 +145,42 @@ public class Folder {
         Folder bdiv = new Folder("bdiv",borgeh);
         Folder btmp = new Folder("btmp",borgeh);
         Folder cdiv = new Folder("cdiv",carsten);
-        System.out.println(home);
+
+        // home.printTree();
+        // System.out.println("Flytter cdiv til borgeh");
+        // cdiv.move(borgeh);
+        // home.printTree();
+        // System.out.println("Flytter borgeh til cdiv");
+        // borgeh.move(cdiv);
+        // home.printTree();   
+
+        // for (int i = 0; i < 30; i++) {
+        //     Folder tmp = new Folder("folder"+i, home);
+        // }
+        // for (Folder folder : home.getFolders()) {
+        //     for (int i = 0; i < 30; i++) {
+        //         Folder tmp = new Folder("folder"+i, folder);
+        //     }    
+        // }
+        // System.out.println(home.contains(cdiv));
+        // System.out.println(home.contains2(cdiv));
+
+        System.out.println("findFirst: "+home.findFirst("borgeh"));
+        File bfil = new File("Børge.txt", borgeh);
+        System.out.println("findFirst fil: "+home.findFirst("Børge.txt"));
+        
+        File hfil = new File("fil.txt", home);
+        File afil = new File("fil.txt", adiv);
+        afil.move(borgeh);
+
+        home.printTree();
+
+        System.out.println(home.findAll("fil.txt"));
+
     }
+
+
+
+
 
 }
